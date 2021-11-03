eb55e803fa4db320737213ef52a5fa46d2f9e76c
Muhammad Ahmad <73386311+0xf10yd@users.noreply.github.com>
2021-11-03 07:15:12 +0200

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCOJtSwvyxKuKSNpLiIUQZ9hzXN6nsgS4Q",
-  authDomain: "trichain-8b6df.firebaseapp.com",
-  projectId: "trichain-8b6df",
-  storageBucket: "trichain-8b6df.appspot.com",
-  messagingSenderId: "500143833562",
-  appId: "1:500143833562:web:56f492a1565fceb49e80e3"
+    apiKey: "AIzaSyCOJtSwvyxKuKSNpLiIUQZ9hzXN6nsgS4Q",
+    authDomain: "trichain-8b6df.firebaseapp.com",
+    projectId: "trichain-8b6df",
+    storageBucket: "trichain-8b6df.appspot.com",
+    messagingSenderId: "500143833562",
+    appId: "1:500143833562:web:56f492a1565fceb49e80e3"
 })
 
 export const auth = app.auth()
